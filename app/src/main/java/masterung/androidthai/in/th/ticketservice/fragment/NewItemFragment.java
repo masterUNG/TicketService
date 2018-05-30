dab9e3aa196350ec5a5dc3748ccf6d8984595281
MasterUNG <phrombutr@gmail.com>
2018-05-30 13:52:12 +0700

    NewItem OK

--- app/src/main/java/masterung/androidthai/in/th/ticketservice/fragment/NewItemFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/ticketservice/fragment/NewItemFragment.java
@@ -4,15 +4,21 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import masterung.androidthai.in.th.ticketservice.R;
 import masterung.androidthai.in.th.ticketservice.utility.MyConstance;
+import masterung.androidthai.in.th.ticketservice.utility.ReadAllData;
+import masterung.androidthai.in.th.ticketservice.utility.ShowListTicketAdapter;
 
-public class NewItemFragment extends Fragment{
+public class NewItemFragment extends Fragment {
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -30,6 +36,41 @@ public class NewItemFragment extends Fragment{
 
             MyConstance myConstance = new MyConstance();
             String urlJSON = myConstance.getUrlNewItemString();
+
+            ReadAllData readAllData = new ReadAllData(getContext());
+            readAllData.execute(urlJSON);
+            String jsonString = readAllData.get();
+            Log.d("30MayV1", "JSON ==> " + jsonString);
+
+            JSONArray jsonArray = new JSONArray(jsonString);
+
+            String[] docNoStrings = new String[jsonArray.length()];
+            String[] serialStrings = new String[jsonArray.length()];
+            String[] detailNoStrings = new String[jsonArray.length()];
+            String[] severityStrings = new String[jsonArray.length()];
+            String[] assignStrings = new String[jsonArray.length()];
+            String[] statusStrings = new String[jsonArray.length()];
+            String[] dueDateStrings = new String[jsonArray.length()];
+
+            for (int i = 0; i < jsonArray.length(); i += 1) {
+
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                docNoStrings[i] = jsonObject.getString("DocNo");
+                serialStrings[i] = jsonObject.getString("SerialNumber");
+                detailNoStrings[i] = jsonObject.getString("Detail");
+                severityStrings[i] = jsonObject.getString("Severity");
+                assignStrings[i] = jsonObject.getString("Assignee");
+                statusStrings[i] = jsonObject.getString("Status");
+                dueDateStrings[i] = jsonObject.getString("DueDate");
+
+            }   // for
+
+            ShowListTicketAdapter showListTicketAdapter = new ShowListTicketAdapter(getActivity(),
+                    docNoStrings, serialStrings, detailNoStrings, severityStrings, assignStrings,
+                    statusStrings, dueDateStrings);
+            listView.setAdapter(showListTicketAdapter);
+
 
 
 
